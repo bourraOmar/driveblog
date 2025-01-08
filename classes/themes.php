1a1bf87edaf709c10cc15b8b28d898b7907c84ec
bourraOmar <bwrtmr@gmail.com>
2025-01-08 17:22:46 +0100

    add class theme

--- classes/themes.php
+++ classes/themes.php
@@ -2,7 +2,8 @@
  require_once '../connection/connect.php';
 
  class Theme {
-    protected $them_nam;
+    protected $them_name;
+    protected $them_desc;
     protected $them_date;
     private $pdo;
 
@@ -11,9 +12,33 @@
       $this->pdo = $connection->connectpdo();
     }
 
-    function ajouterTheme($them_nam, $them_date){
-      $this->them_nam = $them_nam;
+    function ajouterTheme($them_name, $them_desc, $them_date){
+      $this->them_name = $them_name;
+      $this->them_desc = $them_desc;
+      $this->them_date = $them_date;
 
+      $sql = "INSERT INTO theme VALUES(name, description, date_creation)";
+
+      $stmt = $this->pdo->prepare($sql);
+
+      $stmt->bindParam(':nom', $them_name);
+      $stmt->bindParam(':description', $them_desc);
+      $stmt->bindParam(':date_creation', $them_date);
+
+      if($stmt->execute()){
+        header('Location: ../profils/dashboardClient.php');
+        exit();
+      }
+
+    }
+
+    function showTheme(){
+      $sql = "SELECT * FROM theme";
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->execute();
+      if ($stmt->rowCount() > 0){
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+      }
     }
 
  }
